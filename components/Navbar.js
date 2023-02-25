9f541eb69cf0038b5f1d8a32189187eb1db11973
JBarefoot <119702182+JenBarefoot@users.noreply.github.com>
2023-02-25 08:36:26 -0500

    Update Navbar.js
    Added logo image code to see if it works this way

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Navbar() {
   return (
     <nav>
-      <img src="../images/airbnb-logo.png" className="nav--logo" />
+      <img src="https://stackblitz.com/files/react-ts-xd5l4g/github/JenBarefoot/react-airbnb/main/assets/images/airbnb-logo.png
+" className="nav--logo" />
     </nav>
   );
 }
