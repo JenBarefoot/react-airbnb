71b4808692f1ee29cebea56b4086ac43a1f18969
JenBarefoot <119702182+JenBarefoot@users.noreply.github.com>
2023-02-25 08:22:53 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import './style.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Card from './components/Card';
+import data from './data';
+
+export default function App() {
+  const cards = data.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">{cards}</section>
+    </div>
+  );
+}
