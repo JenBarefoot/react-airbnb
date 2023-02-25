c246dfda0a4991c118f3a6ba91046e78ed9060c9
JenBarefoot <119702182+JenBarefoot@users.noreply.github.com>
2023-02-25 08:48:02 -0500

    1 file modified, 6 added, 6 deleted

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,8 +3,7 @@ import React from 'react';
 export default function Navbar() {
   return (
     <nav>
-      <img src="https://stackblitz.com/files/react-ts-xd5l4g/github/JenBarefoot/react-airbnb/main/assets/images/airbnb-logo.png
-" className="nav--logo" />
+      <img src="./images/airbnb-logo.png" className="nav--logo" />
     </nav>
   );
 }
